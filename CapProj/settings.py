038ed3d8a6e557c53a98d386717d00b7ca4f6041
Eva Sawyer <esawyer25@gmail.com>
2018-01-18 10:53:53 -0800

    add os back into settings

--- CapProj/settings.py
+++ CapProj/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-# import os
+import os
 # if 'SECRET_KEY' in os.environ:
 #     pass
 # else:
